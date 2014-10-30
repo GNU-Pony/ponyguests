18e061dfd206277cb83ff7aad78aa7056b68d137
Mattias Andrée <maandree@operamail.com>
2014-10-30 20:09:07 +0100

    whitespace
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/ponyguests-login.c
+++ src/ponyguests-login.c
@@ -147,7 +147,7 @@ int main(int argc, char** argv)
   for (i = 1; i < argc; i++)
     args[i - 1] = argv[i];
   args[argc - 1] = NULL;
-
+  
   /* Create guest account. */
   if (do_start(username) < 0)
     return 1;
